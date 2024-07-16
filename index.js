d5f80499017c4f552138af0b8700825e9ff8086d
Louis Mazin <mazinlouis79@gmail.com>
2024-07-16 17:37:45 +0200

    test

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// bot.js
 const axios = require('axios');
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.GuildMessages] });
@@ -13,8 +12,33 @@ const headers = {
 client.on('ready', () => {
   console.log('Observer started !');
 });
-
-const checkAndRestartServer = async () => {
+const update = async () => {
+    check_state();
+    check_players();
+}
+const check_players = async () => {
+    try {
+        let config = {
+            method: 'get',
+          maxBodyLength: Infinity,
+            url: 'http://localhost:8212/v1/api/metrics',
+            headers: { 
+              'Accept': 'application/json'
+            }
+          };
+          
+          axios(config)
+          .then((response) => {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    } catch (error) {
+        console.error("Une erreur s'est produite lors de la vérification des joueurs :", error);
+    }
+}
+const check_state = async () => {
     try {
         const state_reponse = await fetch("https://panel.louismazin.ovh/api/client/servers/c1e3ad72/resources", { method : "GET", headers });
         const state_data = await state_reponse.json();
@@ -29,4 +53,4 @@ const checkAndRestartServer = async () => {
 };
 client.login(token);
 
-setInterval(checkAndRestartServer, 1000);
+setInterval(update, 1000);
