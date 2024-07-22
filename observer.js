ce3e9de51fa053b1ea93a405ddc5414d1db0daff
Louis Mazin <mazinlouis79@gmail.com>
2024-07-22 16:24:37 +0200

    data issue correction

--- observer.js
+++ observer.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-let restart = {method: 'post',maxBodyLength: Infinity,url: 'http://play.louismazin.ovh:1025/v1/api/shutdown',headers: { 'Accept': 'application/json', 'Authorization': 'Basic YWRtaW46Y2FjYXBpcGlkdTc5'},data : data};
+let restart = {method: 'post',maxBodyLength: Infinity,url: 'http://play.louismazin.ovh:1025/v1/api/shutdown',headers: { 'Accept': 'application/json', 'Authorization': 'Basic YWRtaW46Y2FjYXBpcGlkdTc5'},data : JSON.stringify({"waittime": 10,"message": "La mémoire est pleine, le serveur redémarre dans 10 secondes. (ça prend 20 secondes)" })};
 let save = {method: 'post',maxBodyLength: Infinity,url: 'http://play.louismazin.ovh:1025/v1/api/save',headers: {'Accept': 'application/json', 'Authorization': 'Basic YWRtaW46Y2FjYXBpcGlkdTc5'}};
 let check_ram = {method: 'get',maxBodyLength: Infinity,url: 'https://panel.louismazin.ovh/api/client/servers/c1e3ad72/resources',headers: {"Accept": "application/json","Content-Type": "application/json","Authorization": "Bearer "+process.argv[3].toString()}};
 const checkAndRestartServer = async () => {
