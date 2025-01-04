595e1cc755c539c01ba2eacb65636621bc86a195
Louis Mazin <mazinlouis79@gmail.com>
2025-01-04 18:30:58 +0100

    test

--- commands/roles/roles.js
+++ commands/roles/roles.js
@@ -7,9 +7,7 @@ module.exports = {
 		.setName('roles')
 		.setDescription('Affichez le message qui permet de choisir un rôle !'),
 	async execute(interaction) {
-        const memberTarget = interaction.guild.members.cache.get(interaction.member.user.id);
-        console.log(memberTarget);
-        if (memberTarget.roles.cache.some(role => role.name === 'Modo' || role.name== 'Boss')) {
+        if (interaction.member.user.roles.cache.some(role => role.name === 'Modo' || role.name== 'Boss')) {
             await interaction.reply(rolesMessage);
         }else{
             await interaction.reply("Vous n'avez pas les permissions pour utiliser cette commande !")
