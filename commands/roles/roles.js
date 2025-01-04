90e6f36d15cc917a751e8a060f6ab920fd71cdb0
Louis Mazin <mazinlouis79@gmail.com>
2025-01-04 18:10:56 +0100

    test

--- commands/roles/roles.js
+++ commands/roles/roles.js
@@ -7,6 +7,7 @@ module.exports = {
 		.setName('roles')
 		.setDescription('Affichez le message qui permet de choisir un rôle !'),
 	async execute(interaction) {
+        console.log(interaction.member.roles)
         if (interaction.member.roles.some(role => role.name === 'Modo' || role.name== 'Boss')) {
             await interaction.reply(rolesMessage);
         }else{
