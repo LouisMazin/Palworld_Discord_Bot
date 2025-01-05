52a69b170a210afe496c12f58f260f684e2e21fa
Louis Mazin <mazinlouis79@gmail.com>
2025-01-06 00:59:02 +0100

    fix

--- commands/server-stats/server-stats.js
+++ commands/server-stats/server-stats.js
@@ -119,10 +119,9 @@ module.exports = {
             const players = await getPlayers(platform);
             const message = new EmbedBuilder()
                 .setColor('#0099ff')
-                // .setAuthor({ name: 'Couteau Suisse', iconURL: 'https://srv.latostadora.com/designall.dll/couteau-suisse---dessin-drole-sketchy--i:141385141697014138520;d:1416970;w:520;b:FFFFFF;m:1.jpg', url: 'louismazin.ovh'})
                 .setTitle('Informations sur le Serveur Palworld '+platform)
                 .setDescription(infos+(players==="" ? "" : players+"\n")+'\n'+params);
-            await interaction.reply({ embeds: [message] });
+            await interaction.reply({ embeds: [message], username: "Couteau Suisse",avatar_url: "https://srv.latostadora.com/designall.dll/couteau-suisse---dessin-drole-sketchy--i:141385141697014138520;d:1416970;w:520;b:FFFFFF;m:1.jpg"});
         } catch (error) {
             await interaction.reply({ content: "Une erreur est survenue : " + error, ephemeral: true });
         }
