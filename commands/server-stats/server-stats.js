31615f82099982f22c0fc19e8ac6fd2de90f4cdb
Louis Mazin <mazinlouis79@gmail.com>
2025-01-06 00:14:04 +0100

    test

--- commands/server-stats/server-stats.js
+++ commands/server-stats/server-stats.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { SlashCommandBuilder } = require('discord.js');
 
-const getStats = (platform) => {
+const getPlayersAndFPS = (platform) => {
     return new Promise((resolve, reject) => {
         let infos = "";
         const port = platform === 'steam' ? '1025' : platform === 'xbox' ? '1032' : '';
@@ -30,7 +30,38 @@ const getStats = (platform) => {
         });
     });
 }
+const getParams = (platform) => {
+    return new Promise((resolve, reject) => {
+        let infos = "";
+        const port = platform === 'steam' ? '1025' : platform === 'xbox' ? '1032' : '';
+        
+        if(port === '') {
+            reject('Invalid platform');
+            return;
+        }
 
+        axios({
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://play.louismazin.ovh:'+port+'/v1/api/settings',
+            headers: { 
+                'Accept': 'application/json', 
+                'Authorization': 'Basic YWRtaW46Y2FjYXBpcGlkdTc5'
+            }
+        })
+        .then((response) => {
+            // response.data is a dict, so I want a list of key-value pairs
+            const params = Object.entries(response.data);
+            for(const [key, value] of params) {
+                infos += "## "+key+" : "+value+'\n';
+            }
+            resolve(infos);
+        })
+        .catch((error) => {
+            reject("Erreur lors de la récupération des données : " + error);
+        });
+    });
+}
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server-stats')
@@ -46,8 +77,9 @@ module.exports = {
 	async execute(interaction) {
         try {
             const platform = interaction.options.getString('plateforme');
-            const infos = await getStats(platform);
-            await interaction.reply(infos);
+            const infos = await getPlayersAndFPS(platform);
+            const params = await getParams(platform);
+            await interaction.reply(infos+'\n'+params);
         } catch (error) {
             await interaction.reply({ content: "Une erreur est survenue : " + error, ephemeral: true });
         }
