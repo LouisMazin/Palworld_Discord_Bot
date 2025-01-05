e03e09bdf94516df51ce9a647607585b66c12779
Louis Mazin <mazinlouis79@gmail.com>
2025-01-05 22:52:44 +0100

    server-stats added

---
+++ commands/server-stats/server-stats.js
@@ -0,0 +1,41 @@
+// Affiche les stats su serveur Palworld (joueurs en ligne, paramètres..)
+
+const { SlashCommandBuilder } = require('discord.js');
+const statsMessage = require("./statsMessage.json");
+const { get } = require('http');
+// lambda function
+getStats = (platform) => {
+    return new Promise((resolve, reject) => {
+        const port = platform === 'steam' ? '1025' : platform === 'xbox' ?'1032' : '';
+        if(port === '') {
+            reject('Invalid platform');
+        }
+        axios('http://play.louismazin.ovh:'+port+'/v1/api/stats/')
+        .then((response) => {
+        console.log(response);
+        })
+        .catch((error) => {
+        console.log(error);
+        });
+    });
+}
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server-stats')
+		.setDescription('Afichez les informations sur le Serveur Palworld !')
+		.addStringOption(option =>
+			option.setName('plateforme')
+				.setDescription('Choisissez votre plateforme')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Steam', value: 'steam' },
+					{ name: 'Xbox', value: 'xbox' },
+				)),
+	async execute(interaction) {
+		const platform = interaction.options.getString('plateforme');
+		
+		getStats(platform);
+		
+		// await interaction.reply(message);
+	},
+};
