44656890181176c2850a7725fa78215216d1f61b
Louis Mazin <mazinlouis79@gmail.com>
2025-01-06 01:38:50 +0100

    test

--- commands/bienvenue/bienvenue.js
+++ commands/bienvenue/bienvenue.js
@@ -1,17 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
 const welcomeMessage = require("./welcomeMessage.json")
 
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bienvenue')
-		.setDescription('Souhaitez la bienvenue !')
-		.addUserOption(option =>
-			option.setName('user')
-				.setDescription('Utilisateur à mentionner')
-				.setRequired(false)),
+		.setDescription('Souhaitez la bienvenue !'),
 	async execute(interaction) {
-		const user = interaction.options.getUser('user');
-		const message = user ? `||<@${user.id}>|| ${welcomeMessage}` : welcomeMessage;
-		await interaction.reply(message);
+		const message = JSON.parse(welcomeMessage);
+		message.content = interaction.options.getUser('user');
+		await interaction.reply(JSON.stringify(welcomeMessage));
 	},
 };
