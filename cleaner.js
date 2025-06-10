f44c9050d39dd6143e32e95d5968873ca867ddaa
Louis Mazin <mazinlouis79@gmail.com>
2025-06-11 01:35:14 +0200

    fix

--- cleaner.js
+++ cleaner.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const clean = async (client) => {
     try {
-        client.channels.fetch(1324814179101704373n)
+        client.channels.fetch(1382140739001127002n)
           .then(channel => {
             channel.messages.fetch({ limit: 100 })
               .then(messages => {
