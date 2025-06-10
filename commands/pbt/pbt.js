11074acbe3468fa3097078faf2623b0ecce3f899
Louis Mazin <mazinlouis79@gmail.com>
2025-06-11 00:49:00 +0200

    change message

--- commands/pbt/pbt.js
+++ commands/pbt/pbt.js
@@ -1,6 +1,31 @@
 const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
 const programMessage = require("./programMessage.json")
 
+const getDownloadCount = async () => {
+    try {
+        const response = await axios.get('https://api.github.com/repos/LouisMazin/Palworld_Breeding_Tree/releases', {
+            headers: {
+                'Accept': 'application/vnd.github.v3+json',
+                'User-Agent': 'Palworld-Discord-Bot'
+            }
+        });
+        
+        let totalDownloads = 0;
+        
+        // Sum up downloads from all releases
+        for (const release of response.data) {
+            for (const asset of release.assets) {
+                totalDownloads += asset.download_count;
+            }
+        }
+        
+        return totalDownloads;
+    } catch (error) {
+        console.log('Erreur lors de la récupération du nombre de téléchargements:', error.message);
+        return null;
+    }
+};
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,8 +36,18 @@ module.exports = {
 				.setDescription('Utilisateur à mentionner')
 				.setRequired(false)),
 	async execute(interaction) {
-		user = interaction.options.getUser('user');
-		programMessage.content = user ? "||<@"+user.id+">||" : "";
-		await interaction.reply(programMessage);
+		const user = interaction.options.getUser('user');
+		const downloadCount = await getDownloadCount();
+		
+		// Clone the message to avoid modifying the original
+		const message = JSON.parse(JSON.stringify(programMessage));
+		
+		// Add download count to the embed description
+		if (downloadCount !== null) {
+			message.description = message.description.replace("X", downloadCount.toString()).replace("Y", (downloadCount + 1).toString());
+		}
+		
+		message.content = user ? "||<@"+user.id+">||" : "";
+		await interaction.reply(message);
 	},
 };
